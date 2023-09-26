d1ad5284e9aefecb1ac1d10833a257baa32e346d
Patrick Ly <patrickly7@yahoo.com>
2023-09-26 14:45:29 -0400

    initial discrepancy changes

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -36,7 +36,47 @@ const cardArts = [
 	"res://Images/CardArt/Cloud.png"
 ]
 
+const bends = [
+	"res://Images/Discrep/Bend_SE.png",
+	"res://Images/Discrep/Bend_SW.png"
+]
+
+const damage = "res://Images/Discrep/Damage_HolePunch.png"
+
+const edgeWears = [
+	"res://Images/Discrep/EdgeWear_Bottom.png",
+	"res://Images/Discrep/EdgeWear_Left.png",
+	"res://Images/Discrep/EdgeWear_Right.png",
+	"res://Images/Discrep/EdgeWear_Top.png"
+]
+
+const miscut = "res://Images/Discrep/Miscut.png"
+
+const scratches = [
+	"res://Images/Discrep/Scratch_NW_SE.png",
+	"res://Images/Discrep/Scratch_N_S.png",
+	"res://Images/Discrep/Scratch_SW_NE.png"
+]
+
+const scribbles = [
+	"res://Images/Discrep/Scribble_Jon.png",
+	"res://Images/Discrep/Scribble_Orange.png",
+	"res://Images/Discrep/Scribble_Patrick.png",
+	"res://Images/Discrep/Scribble_Purple.png",
+	"res://Images/Discrep/Scribble_Rose.png"
+]
+
+const smudges = [
+	"res://Images/Discrep/Smudge_Dirt.png",
+	"res://Images/Discrep/Smudge_Fingerprint.png"
+]
+
+var currentCorrectDiscrepancyType = "Condition"
+var currentCorrectCondition = "Near Mint"
+
 func _ready():
+	rng.randomize()
+	
 	# Give time for GET READY text
 	yield(get_tree().create_timer(2.0), "timeout")
 	
@@ -120,6 +160,10 @@ func _on_SubmitButton_pressed():
 	cardsRemaining -= 1
 
 func generateCard():
+	generateCardStyling()
+	generateCardImperfections()
+	
+func generateCardStyling():
 	var randomCardNameIndex = rng.randi_range(0, 2)
 	$Node2D/CurrentCardFront/CardName.texture = load(cardNames[randomCardNameIndex])
 	
@@ -134,7 +178,120 @@ func generateCard():
 	
 	var randomCardArtIndex = rng.randi_range(0, 4)
 	$Node2D/CurrentCardFront/CardArt.texture = load(cardArts[randomCardArtIndex])
+
+func generateCardImperfections():	
+	currentCorrectDiscrepancyType = "Condition"
 	
+	var numScratches = rng.randi_range(0, 3) # 0 - 3 scratches
+	var numSmudges = rng.randi_range(0, 1) # 0 - 1 smudges
+	var numWears = rng.randi_range(0, 4) # 0 - 4 wears
+	var numBends = rng.randi_range(0, 2) # 0 - 2 bends
+	
+	var totalImperfections = numScratches + numSmudges + numWears + numBends
+	match totalImperfections:
+		[0, 1, 2]:
+			currentCorrectCondition = "Near Mint"
+		[3, 4]:
+			currentCorrectCondition = "Lightly Played"
+		[5, 6]:
+			currentCorrectCondition = "Moderately Played"
+		[7, 8]:
+			currentCorrectCondition = "Heavily Played"
+		_:
+			currentCorrectCondition = "Damaged"
+	
+	### Imperfections ###
+	match numSmudges:
+		1:
+			$Node2D/CurrentCardFront/SmudgeFingerprint.show()
+		_:
+			$Node2D/CurrentCardFront/SmudgeFingerprint.hide()
+	
+	match numScratches:
+		1:
+			$Node2D/CurrentCardFront/ScratchNS.hide()
+			$Node2D/CurrentCardFront/ScratchNwSe.hide()
+			$Node2D/CurrentCardFront/ScratchSwNe.show()
+		2:
+			$Node2D/CurrentCardFront/ScratchNS.hide()
+			$Node2D/CurrentCardFront/ScratchNwSe.show()
+			$Node2D/CurrentCardFront/ScratchSwNe.show()
+		3:
+			$Node2D/CurrentCardFront/ScratchNS.show()
+			$Node2D/CurrentCardFront/ScratchNwSe.show()
+			$Node2D/CurrentCardFront/ScratchSwNe.show()
+		_:
+			$Node2D/CurrentCardFront/ScratchNS.hide()
+			$Node2D/CurrentCardFront/ScratchNwSe.hide()
+			$Node2D/CurrentCardFront/ScratchSwNe.hide()
+	
+	match numWears:
+		1:
+			$Node2D/CurrentCardFront/EdgeWearTop.hide()
+			$Node2D/CurrentCardFront/EdgeWearBottom.hide()
+			$Node2D/CurrentCardFront/EdgeWearLeft.hide()
+			$Node2D/CurrentCardFront/EdgeWearRight.show()
+		2:
+			$Node2D/CurrentCardFront/EdgeWearTop.hide()
+			$Node2D/CurrentCardFront/EdgeWearBottom.hide()
+			$Node2D/CurrentCardFront/EdgeWearLeft.show()
+			$Node2D/CurrentCardFront/EdgeWearRight.show()
+		3:
+			$Node2D/CurrentCardFront/EdgeWearTop.hide()
+			$Node2D/CurrentCardFront/EdgeWearBottom.show()
+			$Node2D/CurrentCardFront/EdgeWearLeft.show()
+			$Node2D/CurrentCardFront/EdgeWearRight.show()
+		4:
+			$Node2D/CurrentCardFront/EdgeWearTop.show()
+			$Node2D/CurrentCardFront/EdgeWearBottom.show()
+			$Node2D/CurrentCardFront/EdgeWearLeft.show()
+			$Node2D/CurrentCardFront/EdgeWearRight.show()
+		_:
+			$Node2D/CurrentCardFront/EdgeWearTop.hide()
+			$Node2D/CurrentCardFront/EdgeWearBottom.hide()
+			$Node2D/CurrentCardFront/EdgeWearLeft.hide()
+			$Node2D/CurrentCardFront/EdgeWearRight.hide()
+	
+	match numBends:
+		1:
+			$Node2D/CurrentCardFront/BendSw.hide()
+			$Node2D/CurrentCardFront/BendSe.show()
+		2:
+			$Node2D/CurrentCardFront/BendSw.show()
+			$Node2D/CurrentCardFront/BendSe.show()
+		_:
+			$Node2D/CurrentCardFront/BendSw.hide()
+			$Node2D/CurrentCardFront/BendSe.hide()
+	
+	### Damaged Imperfections ###
+	var damageChance = rng.randi_range(1, 10) # 1/10 chance for damage
+	if (damageChance == 1):
+		$Node2D/CurrentCardFront/Damage.show()
+		currentCorrectCondition = "Damaged"
+	else:
+		$Node2D/CurrentCardFront/Damage.hide()
+		
+	var scribbleChance = rng.randi_range(1, 10) # 1/10 chance for scribbles
+	if (scribbleChance == 1):
+		var randomScribbleIndex = rng.randi_range(0, len(scribbles) - 1)
+		$Node2D/CurrentCardFront/Scribble.texture = load(scribbles[randomScribbleIndex])
+		
+		$Node2D/CurrentCardFront/Scribble.show()
+		currentCorrectCondition = "Damaged"
+	else:
+		$Node2D/CurrentCardFront/Scribble.hide()
+	
+	### Defect ###
+	var miscutChance = rng.randi_range(1, 10) # 1/10 chance for miscut
+	if (miscutChance == 1):
+		$Node2D/CurrentCardBack.texture = load(miscut)
+		currentCorrectDiscrepancyType = "Defect"
+	else:
+		$Node2D/CurrentCardBack.texture = load(cardBack)
+		
+	
+	print(totalImperfections, currentCorrectCondition)
+
 func endGame():
 	Global.timeRemaining = floor($TimerNode/Timer.time_left)
 	$TimerNode/Timer.stop()
@@ -152,7 +309,32 @@ func flipCard():
 		$Node2D/CurrentCardBack.hide()
 		
 func determineSuccessfulProcessing():
+	var currDiscrepancyTypeId = $ProcessCardActions/DiscrepancySelect.get_selected_id()
+	var currDiscrepancyType = $ProcessCardActions/DiscrepancySelect.get_item_text(currDiscrepancyTypeId)
+	
+	# Do Nothing if it's not correct
+	if (currDiscrepancyType != currentCorrectDiscrepancyType):
+		return
+	
+	if (currDiscrepancyType == "Condition"):
+		var currConditionTypeId = $ProcessCardActions/ConditionSelect.get_selected_id()
+		var currCondition = $ProcessCardActions/ConditionSelect.get_item_text(currConditionTypeId)
+		
+		if (currCondition != currentCorrectCondition):
+			return
+	
 	Global.cardsSuccessfullyProcessed += 1
 
 func _on_PauseMenu_hide():
 	$PauseButton.show()
+
+
+#omit dirt
+#
+#scratches anywhere
+#scribbles anywhere
+#hole anywhere
+#smudge anywhere
+#miscut just replace back texture
+#edgewear in specific spots
+#bend in specific spots 
