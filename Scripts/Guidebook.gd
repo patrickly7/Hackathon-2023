5a93cfd9892365e81351b4a516c80cbca8963c22
Patrick Ly <patrickly7@yahoo.com>
2023-09-27 12:37:16 -0400

    added arrow navigation for guidebook

--- Scripts/Guidebook.gd
+++ Scripts/Guidebook.gd
@@ -6,3 +6,17 @@ func _on_CloseButton_pressed():
 
 func _on_TabContainer_tab_changed(tab):
 	$BackgroundSFX.play()
+
+func _on_LeftArrow_pressed():
+	if ($TabContainer.current_tab == 0):
+		return
+		
+	$TabContainer.current_tab -= 1
+	$BackgroundSFX.play()
+
+func _on_RightArrow_pressed():
+	if ($TabContainer.current_tab == $TabContainer.get_tab_count() - 1):
+		return
+		
+	$TabContainer.current_tab += 1
+	$BackgroundSFX.play()
