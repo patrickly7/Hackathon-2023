219d79d3fb5ad0af3e578c785cefaecc3c667554
Patrick Ly <patrickly7@yahoo.com>
2023-09-27 11:12:40 -0400

    Updated results with flavor text

--- Scripts/Results.gd
+++ Scripts/Results.gd
@@ -15,6 +15,7 @@ func _ready():
 	yield(get_tree().create_timer(1.0), "timeout")
 	$PlayAgainButton.show()
 	$StartMenuButton.show()
+	$FlavorText.show()
 
 func _on_PlayAgainButton_pressed():
 	resetGlobalVariables()
@@ -39,27 +40,33 @@ func updateResultsDisplay():
 func calculateRank():
 	# S Rank
 	if (Global.guidebookUses <= 0 && Global.cardsSuccessfullyProcessed >= 20 && Global.timeRemaining > 0):
+		$FlavorText/Label.text = "Amazing!\nWithout the guide, too!"
 		$Ranking.texture = load("res://Images/UI/S-Rank.png")
 		return
 	
 	# A Rank
 	if (Global.cardsSuccessfullyProcessed >= 20 && Global.timeRemaining > 0):
+		$FlavorText/Label.text = "Great work!\nYou know your stuff!"
 		$Ranking.texture = load("res://Images/UI/A-Rank.png")
 		return
 	
 	# B Rank
 	if (Global.cardsSuccessfullyProcessed >= 15):
+		$FlavorText/Label.text = "Good job!\nYou almost got them all!"
 		$Ranking.texture = load("res://Images/UI/B-Rank.png")
 		return
 	
 	# C Rank
 	if (Global.cardsSuccessfullyProcessed >= 10):
+		$FlavorText/Label.text = "You're getting better.\nKeep it up!"
 		$Ranking.texture = load("res://Images/UI/C-Rank.png")
 		return
 	
 	# D Rank
 	if (Global.cardsSuccessfullyProcessed >= 5):
+		$FlavorText/Label.text = "Don't worry.\nWe all start somewhere."
 		$Ranking.texture = load("res://Images/UI/D-Rank.png")
 		return
 	
+	$FlavorText/Label.text = "Next time, read the guidebook.\nYou need to study more."
 	$Ranking.texture = load("res://Images/UI/F-Rank.png")
